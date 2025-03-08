c588df978710d8601ac2a2b4f5e1ad3418bc67cc
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-03-08 13:34:07 +0545

    data transformation started

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -12,5 +12,14 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 
-
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts',"preprocessor.pkl")
+    
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformation()
+        
+    def get_data_transformer_object(self):
+        pass
 
