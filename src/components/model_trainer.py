9dcba621d365f2cad2cb65392563179c80af9a82
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-03-09 10:57:21 +0545

    continuing model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,7 +17,7 @@ from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object
+from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -29,7 +29,34 @@ class ModelTrainer:
         
     def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
         try:
-            pass
+            logging.info("Splitting training and test input data")
+            
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+            
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            
+            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            
+            # get the best model score from the dict
+            best_model_score = max(sorted(model_report.values))
+            
+            # get the best model name from the dict
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
         except:
             pass
 
