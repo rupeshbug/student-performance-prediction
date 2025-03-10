77c4ee4bf13f40c3bdf1d33cd03d8de5ebd2fdd3
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-03-10 08:36:02 +0545

    best model saved

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,7 +48,7 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
             
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
             
             # get the best model score from the dict
             best_model_score = max(sorted(model_report.values))
@@ -57,6 +57,19 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
+            
+            best_model = models[best_model_name]
+            
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            
+            logging.info(f"Best model found on both training and testing dataset")
+            
+            save_object(
+                file_path = self.model_trainer_config.trained_model_file_path,
+                obj = best_model
+            )
+            
         except:
             pass
 
