10872ea020b4dbd8678b3a01de3d2110ce402886
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-03-11 08:44:36 +0545

    home page setup

--- app.py
+++ app.py
@@ -11,3 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/predictdata', method = ["GET", "POST"])
+def predict_datapoint():
+    if request.method == "GET":
+        return render_template('home.html')
+    else:
+        pass
