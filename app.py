194690f1fad9663aca4c40e37318ed90e11f9647
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-03-11 08:41:05 +0545

    flask for web app started

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+
+# route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html')
