e60c38fbe01397881794a8c0d469136f18935749
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-03-14 11:32:17 +0545

    minor changes

--- src/exception.py
+++ src/exception.py
@@ -14,7 +14,7 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
         
-        # Log the error message to the logs file
+        # Log the error message to the logs file in the defined structure
         logging.error(self.error_message)
 
     def __str__(self):
